1fe81989a3781d6c660fded8b768039294a6f5fa
Rufina Gafiiatullina <r.gafiiatullina@innopolis.university>
2025-12-02 19:07:33 +0300

    Change migrations/env.py

--- app/migrations/env.py
+++ app/migrations/env.py
@@ -44,7 +44,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    # url = config.get_main_option("sqlalchemy.url")
+    url = config.get_main_option("sqlalchemy.url")
 
     context.configure(
         url=url,
