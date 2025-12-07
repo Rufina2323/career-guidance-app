844cedb7abf31f795c07bafc71f21692082db218
Rufina Gafiiatullina <r.gafiiatullina@innopolis.university>
2025-12-07 23:54:14 +0300

    Add endpoints

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
-from fastapi import FastAPI, HTTPException, Request
+import uuid
+from dtos.update_balance_dto import UpdateBalanceDTO
+from dtos.user_login_dto import UserLoginDTO
+from dtos.register_person_dto import RegisterPersonDTO, RegisterRole
+from fastapi import FastAPI, HTTPException, Request, Response, Depends
 from fastapi.responses import JSONResponse
+
+from authx import AuthX, AuthXConfig
 
 from create_entites.person.impl.admin import AdminCreateEntity
 from create_entites.person.impl.user import UserCreateEntity
@@ -23,11 +29,33 @@ app = FastAPI(
     title="Сервисное API", description="API для управления событиями", version="1.0.0"
 )
 
+auth_config = AuthXConfig()
+auth_config.JWT_SECRET_KEY = "SECRETE_KEY"
+auth_config.JWT_ACCESS_COOKIE_NAME = "my_access_token"
+auth_config.JWT_TOKEN_LOCATION = ["cookies"]
+
+security = AuthX(config=auth_config)
+
 ml_request_service = MLRequestService()
 balace_service = BalanceService()
 user_service = UserService()
 admin_service = AdminService()
 transaction_service = TransactionService()
+
+
+@app.post("/login")
+def login(user_login_dto: UserLoginDTO, response: Response):
+    person = user_service.authenticate_person(user_login_dto.username, user_login_dto.password)
+    if person:
+        token = security.create_access_token(uid=str(person.user_id))
+        response.set_cookie(auth_config.JWT_ACCESS_COOKIE_NAME, token)
+        return {"access_token": token}
+    raise HTTPException(status_code=401, detail="Incorrect username or password")
+
+
+@app.get("/protected")
+def protected(login_user = Depends(security.access_token_required)):
+    return login_user
 
 
 @app.get("/", response_model=dict[str, str])
@@ -59,23 +87,41 @@ async def health_check() -> dict[str, str]:
     return {"status": "healthy"}
 
 
-@app.post("/demo")
-def demo() -> None:
-    user_create_entity = UserCreateEntity(
-        username="Anton", email="example123@gmail.com", password="hard_password"
-    )
-    user = user_service.add_person(user_create_entity)
+@app.post("/register")
+def register(register_person_dto: RegisterPersonDTO) -> dict[str, str]:
+    if register_person_dto.role == RegisterRole.USER:
+        success, result = user_service.add_person(
+            UserCreateEntity(
+                username=register_person_dto.username,
+                email=register_person_dto.email,
+                password=register_person_dto.password
+            )
+        )
+    else:
+        success, result = admin_service.add_person(
+            AdminCreateEntity(
+                username=register_person_dto.username,
+                email=register_person_dto.email,
+                password=register_person_dto.password
+            )
+        )
+    if not success:
+        raise HTTPException(status_code=400, detail=result)
+    return {"message": "Admin created successfully", "user_id": str(result.user_id)}
+    
 
-    admin_create_entity = AdminCreateEntity(
-        username="Galina", email="blanka@gmail.com", password="tasty_soup"
-    )
-    admin_service.add_person(admin_create_entity)
-
-    admin_service.deposit_to_user(user, 600)
-
-    user = user_service.get_person(user.user_id)
-
-    assert user.balance.amount == 600
+@app.get("/balance")
+def get_balance(user_id: uuid.UUID) -> dict[str, float]:
+    balance = user_service.get_balance(user_id)
+    if not balance:
+            raise HTTPException(status_code=400, detail="user_id does not exists")
+    return {"balance": balance.amount}
+    
+    
+@app.put("/balance")
+def update_balance(user_id: uuid.UUID, update_balance_dto: UpdateBalanceDTO) -> None:
+    user = user_service.get_person(user_id)
+    admin_service.deposit_to_user(user, update_balance_dto.amount)
 
 
 # Обработчики ошибок
