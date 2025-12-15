ec3fdd202f4663b0dadb9145e0f1e63a1887fe7c
Rufina Gafiiatullina <r.gafiiatullina@innopolis.university>
2025-12-15 22:00:04 +0300

    Add ml model repository

---
+++ app/repositories/ml_model/impl/ml_model_psql_repository.py
@@ -0,0 +1,38 @@
+import uuid
+from entities.ml_model.impl.career_prediction_model import CareerPredictionModel
+from entities.ml_model.ml_model import MLModel
+
+from repositories.ml_model.repository import MLModelRepository
+from sqlmodel import Session, select
+from database.engine import engine
+from models.ml_model import MLModel as MLModelModel
+from sqlalchemy.exc import NoResultFound
+
+
+class MLModelPSQLRepository(MLModelRepository):
+    def get_ml_model_request_cost(self, ml_model_id: uuid.UUID) -> float | None:
+        with Session(engine) as session:
+            try:
+                statement = select(MLModelModel).where(MLModelModel.id == ml_model_id)
+                psql_ml_model = session.exec(statement).one()
+                return psql_ml_model.request_cost
+
+            except NoResultFound:
+                return None
+
+    def get_ml_model(self, ml_model_id: uuid.UUID) -> MLModel:
+        with Session(engine) as session:
+            try:
+                statement = select(MLModelModel).where(MLModelModel.id == ml_model_id)
+                psql_ml_model = session.exec(statement).one()
+                return CareerPredictionModel(
+                    model_id=psql_ml_model.id,
+                    name=psql_ml_model.name,
+                    request_cost=psql_ml_model.request_cost,
+                    model_path=psql_ml_model.model_path,
+                    preprocessing_path=psql_ml_model.preprocessing_path,
+                    label_encoder_path=psql_ml_model.label_encoder_path,
+                )
+
+            except NoResultFound:
+                return None
