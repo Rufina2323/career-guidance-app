7b186b23fed6de9ca998c9da2118145833ce465b
Rufina Gafiiatullina <r.gafiiatullina@innopolis.university>
2025-12-15 22:01:05 +0300

    Update services

--- app/services/ml_model_service.py
+++ app/services/ml_model_service.py
@@ -4,18 +4,47 @@ from entities.ml_model.ml_model import MLModel
 from entities.response_data.impl.career_prediction_model_response_data import (
     CareerPredictionModelResponseData,
 )
+from repositories.ml_model.impl.ml_model_psql_repository import MLModelPSQLRepository
+from repositories.ml_model.repository import MLModelRepository
+from services.response_data_service import ResponseDataService
+import torch
+import pandas as pd
 
 
 class MLModelService:
-    def get_model_cost(self, model_id: uuid.UUID) -> float:
-        return 100
+    def __init__(self) -> None:
+        self.response_data_service = ResponseDataService()
+        self.ml_model_repository: MLModelRepository = MLModelPSQLRepository()
 
-    def preprocess_inference_data(
-        self, ml_model: MLModel, inference_data: InferenceData
-    ) -> InferenceData:
-        pass
+    def get_ml_model_request_cost(self, ml_model_id: uuid.UUID) -> float:
+        return self.ml_model_repository.get_ml_model_request_cost(ml_model_id)
+
+    def get_ml_model(self, ml_model_id: uuid.UUID) -> MLModel:
+        return self.ml_model_repository.get_ml_model(ml_model_id)
 
     def predict(
-        self, ml_model_id: uuid.UUID, inference_data: InferenceData
-    ) -> CareerPredictionModelResponseData:
-        return CareerPredictionModelResponseData(job_role_result="None")
+        self,
+        ml_model_id: uuid.UUID,
+        repsonse_data_id: uuid.UUID,
+        inference_data: InferenceData,
+    ) -> uuid.UUID:
+        ml_model = self.get_ml_model(ml_model_id)
+        inference_data["instrovert"] = "yes" if True else "no"
+        print(inference_data)
+        inference_data_df = pd.DataFrame([inference_data])
+        preprocessed_data = ml_model.preprocessing_pipeline.transform(
+            inference_data_df
+        ).toarray()
+        preprocessed_data_tensor = torch.tensor(preprocessed_data, dtype=torch.float32)
+        with torch.no_grad():
+            logits = ml_model.model(preprocessed_data_tensor)
+            preds = torch.argmax(logits, dim=1)
+
+        predicted_labels = ml_model.label_encoder.inverse_transform(preds.numpy())
+        prediction = CareerPredictionModelResponseData(
+            job_role_result=predicted_labels[0]
+        )
+        repsonse_data_id = self.response_data_service.add_data(
+            repsonse_data_id, prediction
+        )
+        return repsonse_data_id
--- app/services/ml_request_service.py
+++ app/services/ml_request_service.py
@@ -1,6 +1,10 @@
 import uuid
 from create_entites.inference_data.inference_data import InferenceDataCreateEntity
 from entities.ml_request import MLRequest
+from entities.response_data.impl.career_prediction_model_response_data import (
+    CareerPredictionModelResponseData,
+)
+from entities.response_data.response_data import ResponseData
 from repositories.ml_request.impl.ml_request_psql_repository import (
     MLRequestPSQLRepository,
 )
@@ -24,27 +28,36 @@ class MLRequestService:
         user_id: uuid.UUID,
         balance_id: uuid.UUID,
         model_id: uuid.UUID,
+        repsonse_data_id: uuid.UUID,
         inference_data: InferenceDataCreateEntity,
     ) -> uuid.UUID:
-        model_cost = self.ml_model_service.get_model_cost(model_id)
+        model_cost = self.ml_model_service.get_ml_model_request_cost(model_id)
         self.balance_service.can_withdraw(balance_id, model_cost)
 
         inference_data_id = self.inference_data_service.add_data(inference_data)
-        response_data = self.ml_model_service.predict(model_id, inference_data)
-        repsonse_data_id = self.response_data_service.add_data(response_data)
+        self.response_data_service.add_data(
+            repsonse_data_id, CareerPredictionModelResponseData(job_role_result="")
+        )
 
         ml_request_id = self.ml_request_repository.create_ml_request(
             model_cost, user_id, model_id, inference_data_id, repsonse_data_id
         )
 
         self.balance_service.withdraw(ml_request_id, balance_id, model_cost)
-
         return ml_request_id
 
+    def finish_ml_request(self, ml_request_id: uuid.UUID) -> None:
+        self.ml_request_repository.finish_ml_request(ml_request_id)
+
+    def get_user_id(self, ml_request_id: uuid.UUID) -> uuid.UUID:
+        return self.ml_request_repository.get_user_id(ml_request_id)
+
+    def get_prediction(self, ml_request_id: uuid.UUID) -> ResponseData:
+        response_data_id = self.ml_request_repository.get_prediction_id(ml_request_id)
+        return self.response_data_service.get_data(response_data_id)
+
     def get_ml_request_history(self, user_id: uuid.UUID) -> list[MLRequest]:
-        ml_requests = self.ml_request_repository.get_ml_request_history(
-            user_id
-        )  # key= lambda t: t.timestamp, reverse=True)
+        ml_requests = self.ml_request_repository.get_ml_request_history(user_id)
 
         ml_requests.sort(key=lambda ml_request: ml_request.timestamp, reverse=True)
 
