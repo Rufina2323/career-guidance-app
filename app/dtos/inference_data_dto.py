a1e45d9d6f64cc7ffa8eb8f9a5a792fde8ed00c2
Rufina Gafiiatullina <r.gafiiatullina@innopolis.university>
2025-12-15 21:57:01 +0300

    Add full inference data dto

--- app/dtos/inference_data_dto.py
+++ app/dtos/inference_data_dto.py
@@ -1,8 +1,40 @@
 from pydantic import BaseModel
 
 
-class InderenceDataDTO(BaseModel):
+class InferenceDataDTO(BaseModel):
     operating_systems_percentage: int
     algorithms_percentage: int
     programming_concepts_percentage: int
     software_engineering_percentage: int
+    electronics_subjects_percentage: int
+    computer_architecture_percentage: int
+    mathematics_percentage: int
+    communication_skills_percentage: int
+    hours_working_per_day: int
+    logical_quotient_rating: int
+    hackathons: int
+    coding_skills_rating: int
+    public_speaking_points: int
+    can_work_long_time: str
+    self_learning_capability: str
+    extra_courses_did: str
+    certifications: str
+    workshops: str
+    talent_tests_taken: str
+    olympiads: str
+    reading_writing_skills: str
+    memory_capability_score: str
+    interested_subjects: str
+    interested_career_area: str
+    job_higher_studies: str
+    company_type_prefered: str
+    taken_inputs_from_elders: str
+    interested_in_games: str
+    interested_book_types: str
+    salary_range_expected: str
+    in_realtionship: str
+    behaviour: str
+    management_or_technical: str
+    worker_type: str
+    team_work: str
+    instrovert: bool
