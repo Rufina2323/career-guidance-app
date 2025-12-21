f4c28f3d9432329578e7380a00d297d26b042576
Rufina Gafiiatullina <r.gafiiatullina@innopolis.university>
2025-12-21 14:13:50 +0300

    Update ml worker

--- app/worker.py
+++ app/worker.py
@@ -2,22 +2,29 @@ import json
 import uuid
 import pika
 from messaging.rabbitmq import get_connection
+from models.ml_request import Status
 from services.ml_model_service import MLModelService
+from services.ml_request_service import MLRequestService
 
 
 def callback(ch, method, properties, body):
     data = json.loads(body)
     ml_model_service = MLModelService()
+    ml_request_service = MLRequestService()
 
     try:
+        ml_request_service.change_ml_request_status(ml_request_id=data["request_id"], ml_request_status=Status.RUNNING)
         prediction = ml_model_service.predict(
             uuid.UUID(data["ml_model_id"]),
             uuid.UUID(data["response_data_id"]),
             data["payload"],
         )
         print(prediction, flush=True)
+        ml_request_service.change_ml_request_status(ml_request_id=data["request_id"], ml_request_status=Status.COMPLETED)
         ch.basic_ack(delivery_tag=method.delivery_tag)
     except Exception as e:
+        ml_request_service.change_ml_request_status(ml_request_id=data["request_id"], ml_request_status=Status.FAILED)
+        ml_request_service.fail_ml_request(ml_request_id=data["request_id"])
         print("Error:", e)
         ch.basic_nack(delivery_tag=method.delivery_tag, requeue=False)
 
