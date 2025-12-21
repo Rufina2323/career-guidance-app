d46f267b91626283419e179de903446af46c4ce8
Rufina Gafiiatullina <r.gafiiatullina@innopolis.university>
2025-12-21 14:13:37 +0300

    Update ml request service

--- app/services/ml_request_service.py
+++ app/services/ml_request_service.py
@@ -5,6 +5,7 @@ from entities.response_data.impl.career_prediction_model_response_data import (
     CareerPredictionModelResponseData,
 )
 from entities.response_data.response_data import ResponseData
+from models.ml_request import Status
 from repositories.ml_request.impl.ml_request_psql_repository import (
     MLRequestPSQLRepository,
 )
@@ -13,11 +14,13 @@ from services.balance_service import BalanceService
 from services.inference_data_service import InferenceDataService
 from services.ml_model_service import MLModelService
 from services.response_data_service import ResponseDataService
+from services.user_service import UserService
 
 
 class MLRequestService:
     def __init__(self) -> None:
         self.balance_service = BalanceService()
+        self.user_service = UserService()
         self.ml_model_service = MLModelService()
         self.inference_data_service = InferenceDataService()
         self.response_data_service = ResponseDataService()
@@ -45,12 +48,22 @@ class MLRequestService:
 
         self.balance_service.withdraw(ml_request_id, balance_id, model_cost)
         return ml_request_id
+    
+    def fail_ml_request(self, ml_request_id: uuid.UUID) -> None:
+        user_id = self.ml_request_repository.get_user_id(ml_request_id)
+        balance_id = self.user_service.get_user_balance_id(user_id)
+        model_cost = self.ml_request_repository.get_ml_request_cost(ml_request_id)
+        self.balance_service.deposit(balance_id, model_cost)
 
-    def finish_ml_request(self, ml_request_id: uuid.UUID) -> None:
-        self.ml_request_repository.finish_ml_request(ml_request_id)
+
+    def change_ml_request_status(self, ml_request_id: uuid.UUID, ml_request_status: Status) -> None:
+        self.ml_request_repository.change_ml_request_status(ml_request_id, ml_request_status)
 
     def get_user_id(self, ml_request_id: uuid.UUID) -> uuid.UUID:
         return self.ml_request_repository.get_user_id(ml_request_id)
+    
+    def get_ml_request_status(self, ml_request_id: uuid.UUID) -> Status:
+        return self.ml_request_repository.get_ml_request_status(ml_request_id)
 
     def get_prediction(self, ml_request_id: uuid.UUID) -> ResponseData:
         response_data_id = self.ml_request_repository.get_prediction_id(ml_request_id)
